4c43858ce6a21666b5fd1b056c53532f369222dd
Ikem Okonkwo <ikem.okonkwo@andela.com>
2016-04-10 19:40:31 +0100

    feat(mode): modify game mode. Better flow of game

--- apps/bs_client/lib/bs_client/game/mode.ex
+++ apps/bs_client/lib/bs_client/game/mode.ex
@@ -1,23 +1,41 @@
 defmodule BSClient.Game.Mode do
-  def setup do
+  alias BSClient.ServerProcotol
+
+  def setup(args) do
     IO.puts Message.mode
     IO.gets("Choose any mode to continue: ")
     |> String.rstrip(?\n)
-    |> command
+    |> command(args)
   end
 
-  defp command(value) when value in ["h", "human"] do
-    [ opponent: :human ]
+  defp command(value, args) when value in ["h", "human"] do
+    nick = IO.gets("Enter opponent nick: ") |> String.rstrip(?\n)
+    ServerProcotol.request_game(args)
+    |> player(nick)
   end
 
-  defp command(value) when value in ["c", "computer"] do
+  defp command(value, args) when value in ["c", "computer"] do
     [ opponent: :computer ]
   end
 
-  defp command(value) do
+  defp command(value, args) do
     IO.puts """
     Command you entered is wrong. Enter a valid command.
     You can enter h, human, c or computer
     """
+  end
+
+  defp player(:accepted, nick) do
+    [ opponent: :human, name: nick ]
+  end
+
+  defp player(:declined, _nick) do
+    IO.puts "Opponent declined the offer to play."
+    setup
+  end
+
+  defp player(:player_not_online, _nick) do
+    IO.puts "The player with the nick you selected is not online."
+    setup
   end
 end
